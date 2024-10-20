e762e5c7f2454eb14fe3e110ffe6761cfeae8558
ChinmayeeJuturu <juturuchinmayee123@gmail.com>
2024-10-20 13:37:16 +0530

    commited

--- Student-Mentoring-System-main/client/src/index.js
+++ Student-Mentoring-System-main/client/src/index.js
@@ -3,6 +3,7 @@ import ReactDom from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import Kommunicate from "@kommunicate/kommunicate-chatbot-plugin";
 
 // import "tw-elements";
 import reducers from "./reducers/index";
@@ -11,6 +12,10 @@ import "react-toastify/dist/ReactToastify.css";
 import App from "./App";
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
+Kommunicate.init("2691e71639dd5bc66b0aef4d3712ad18a", {
+    automaticChatOpenOnNavigation: true,
+    popupWidget: true
+  });
 
 ReactDom.render(
     <Provider store={store}>
